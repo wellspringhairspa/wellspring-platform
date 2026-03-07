999ee5f29b20898217a67c8087138d2135b699d1
Molly <webstermollygee@gmail.com>
2026-03-06 23:18:47 -0600

    disable eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,18 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "react-hooks/exhaustive-deps": "off"
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
@@ -21,5 +22,4 @@ const eslintConfig = [
     ],
   },
 ];
-
 export default eslintConfig;
